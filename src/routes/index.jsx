569f806c0bcbf581015e722b2ff4e1b5f63ab5f6
Emil Hajizada <emil.ph@code.local>
2022-10-11 16:37:25 +0400

    Bug Fix

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,26 +5,34 @@ import Login from "../pages/site/Login";
 import Register from "../pages/site/Register";
 import AdminRoot from "../pages/admin/AdminRoot";
 
+
 export const ROUTES = [
   {
-    name: "Root Element for web",
-    path: "/",
-    element: <Home />,
-  },
-  {
-    name: "News",
-    path: "news",
-    element: <News />,
-  },
-  {
-    name: "Login",
-    path: "login",
-    element: <Login />,
-  },
-  {
-    name: "Register",
-    path: "register",
-    element: <Register />,
+      name: "Root Element for web",
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          name: "Home",
+          path: "/",
+          element: <Home/>,
+        },
+        {
+          name: "News",
+          path: "news",
+          element: <News/>,
+        },
+        {
+          name: "Login",
+          path: "login",
+          element: <Login />,
+        },
+        {
+          name: "Register",
+          path: "register",
+          element: <Register/>,
+        },
+      ],
   },
   {
     name: "Admin Root",
@@ -39,7 +47,7 @@ export const ROUTES = [
       {
         name: "News",
         path: "news",
-        element: <News />,
+        element: <News/>,
       },
     ],
   },
