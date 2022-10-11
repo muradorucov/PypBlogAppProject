28ff9c7b1e93a8894874b28f13f44238d1a48d7a
Emil Hajizada <emil.ph@code.local>
2022-10-11 15:47:19 +0400

    Feat:Added Navbar

--- src/pages/site/Home/index.jsx
+++ src/pages/site/Home/index.jsx
@@ -1,14 +1,9 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import {ProviderContext} from "../../../context/ContextProvider";
-import { categoryNetwork } from "../../../services/network/requests/categoryNetwork";
 
 function Home() {
-  useEffect(() => {
-    categoryNetwork.getAllCategories()
-  }, [])
   
   const {counter} = useContext(ProviderContext)
-  console.log();
   
   return<>
    <div>Home {counter}</div>
