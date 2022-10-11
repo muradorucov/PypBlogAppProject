40fe37e7e5230e5a81879bca3a576e1058135314
Emil Hajizada <emil.ph@code.local>
2022-10-11 13:04:39 +0400

    Bug fix: router / fixed

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -13,7 +13,7 @@ export const ROUTES = [
         children: [
           {
             name: "Home",
-            path: "home",
+            path: "/",
             element: <Home/>,
           },
           {
